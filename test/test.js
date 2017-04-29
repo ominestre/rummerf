ebfb3f77072faaf8911afed2ef1d7351d0865a21
Mike Miller <ominestre@gmail.com>
2017-04-29 08:29:02 -0400

    Creates foundational unit tests for module requirements

---
+++ test/test.js
@@ -0,0 +1,19 @@
+describe('Deleting', function(){
+    xit('Deletes a single file', function(){
+    
+    });
+
+    xit('Deletes a single depth directory', function(){
+
+    });
+
+    xit('Deletes a multi depth directory', function(){
+
+    });
+});
+
+describe('Project scoping limitations', function(){
+    xit('Throws a file error when you attempt to delete something outside of the root of your project', function(){
+
+    });
+});
