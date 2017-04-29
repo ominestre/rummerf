37324fb4f4950c3a4561ae8cf79f9299b2b8b639
Mike Miller <ominestre@gmail.com>
2017-04-29 12:17:33 -0400

    Creates rummerf module
    This passes the first waves of tests

---
+++ rummerf.js
@@ -0,0 +1,30 @@
+function isProtected(path){
+    if(path === '/')
+        throw new Error('Root is protected');
+    
+    const fs = require('fs');
+    fs.accessSync(path, 7);
+}
+
+module.exports = (function(scope){
+    if(!scope)
+        scope = process.cwd();
+    
+    if(!require('path').isAbsolute(scope))
+        throw new Error('Scope must be an absolute path');
+
+    isProtected(scope);
+
+    return function(target){
+        if(!target)
+            throw new Error('No target provided to rummerf');
+        
+        if(!require('path').isAbsolute(target))
+            throw new Error('Target must be an absolute path');
+        
+        if(!target.includes(scope))
+            throw new Error('Target is outside of defined scope');
+
+        isProtected(target);
+    }
+})();
