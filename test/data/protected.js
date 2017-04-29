ff3362591931016285f3345aaf2d6c82e3fcdd38
Mike Miller <ominestre@gmail.com>
2017-04-29 11:02:57 -0400

    Creates unit tests for default rummerf file scoping

---
+++ test/data/protected.js
@@ -0,0 +1,1 @@
+module.exports = `can't touch this`;
--- test/test.js
+++ test/test.js
@@ -1,3 +1,19 @@
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+
+before(function(done){
+    this.timeout(30000);
+    process.stdout.write('Preping test sandbox for scoping tests...\n');
+    require('child_process').execSync('npm install', {cwd: path.resolve(__dirname, './data/sandbox/')});
+    process.stdout.write('Test environment ready\n\n\n');
+
+    // securing protected file for testing
+    fs.chmodSync(path.resolve(__dirname, './data/protected.js'), 444);
+
+    done();
+});
+
 describe('Deleting', function(){
     xit('Deletes a single file', function(){
     
@@ -17,7 +33,46 @@ describe('Deleting', function(){
 });
 
 describe('Project scoping limitations', function(){
-    xit('Throws a file error when you attempt to delete something outside of the root of your project', function(){
+    const cwd = process.cwd();
 
+    before(() => {
+        // changes current working directory into the sandbox directory
+        process.chdir(path.resolve(__dirname, './data/sandbox/'));
     });
+
+    describe('With defined scoping', function(){
+        xit('Throws error if current working directory is root or protected', function(){
+
+        });
+
+        xit('Throws an error when you attempt to delete a file outside of the defined scope', function(){
+
+        });
+    });
+
+    describe('With default scoping using current working directory', function(){
+        const rummerf = require('../');
+
+        it('Throws error if current working directory is root or protected', function(){
+            assert.throws(() => {
+                rummerf(path.resolve(__dirname, './data/protected.js'));
+            }, /Access Error/);
+        });
+
+        it('Throws an error when you attempt to delete a file outside of default scope', function(){
+            assert.throws(() => {
+                rummerf(path.resolve(__dirname, './data/no-delete.js'));
+            }, /Access Error/);
+        });
+    });
+
+    after(() => {
+        // restores current working directory to test process
+        process.chdir(cwd);
+    });
+});
+
+after(() => {
+    // removing protection for cleanup
+    fs.chmodSync(path.resolve(__dirname, './data/protected.js'), 777);
 });
